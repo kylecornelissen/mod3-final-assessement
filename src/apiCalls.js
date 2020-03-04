abe391dc216d08194785f77d4979612018dd2437
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-03-04 09:33:07 -0700

    Finish Iteration 1 Fix

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,9 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Error fetching ideas');
+      }
+      return response.json();
+    });
 }
