5baa5cb326f5578ee5e0372042e33c7476376d85
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-03-04 10:00:28 -0700

    Finish Iteration 2 Fix

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { getUrls } from '../../apiCalls';
+import { setUrls } from '../../actions';
 import './UrlContainer.css';
 
-const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
-    return (
-      <div className="url">
-        <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
-      </div>
-    )
-  });
+export class UrlContainer extends Component {
+  componentDidMount() {
+    getUrls()
+      .then(data => this.props.setUrls(data.urls))
+      .catch(err => console.error('Error fetching:', err));
+  }
 
-  return (
-    <section>
-      { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
-    </section>
-  )
+  render() {
+    let { urls } = this.props;
+    urls = urls.map(url => {
+      return (
+        <div className="url">
+          <h3>{url.title}</h3>
+          <a href={url.short_url} target="blank">{url.short_url}</a>
+          <p>{url.long_url}</p>
+        </div>
+      )
+    });
+    return (
+      <section>
+        { urls.length ? urls : <p>No urls yet! Find some to shorten!</p> }
+      </section>
+    )
+  }
 }
 
-export default UrlContainer;
+export const mapStateToProps = ({ urls }) => ({
+  urls
+});
+
+export const mapDispatchToProps = dispatch => {
+  return {
+    setUrls: urls => dispatch(setUrls(urls))
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UrlContainer);
