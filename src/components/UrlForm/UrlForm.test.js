68c9f849678f1bf26ea78c00944626f21bc8c0d6
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-03-04 11:53:14 -0700

    Attempted URL Form test

---
+++ src/components/UrlForm/UrlForm.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import URLForm from './URLForm';
+
+describe('URL Form', () => {
+  it('renders the inputs in a form', () => {
+    // const wrapper = shallow(<URLForm />);
+    // const title = <form> </form>;
+    //
+    // expect(wrapper.contains(title)).toEqual(true);
+  });
+});
